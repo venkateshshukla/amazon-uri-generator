02a6438fe3f6cdf2ee851c11314da3ffbcd0a2fd
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2016-12-22 09:10:32 +0530

    Added missing amazonKeys.sh file

---
+++ amazonKeys.sh
@@ -0,0 +1,8 @@
+# Amazon Access Key
+export AWS_ACCESS_KEY_ID='AKIAIOSFODNN7EXAMPLE'
+
+# Amazon Secret Key
+export AWS_SECRET_ACCESS_KEY='1234567890'
+
+# Amazon Associate Tag
+export AWS_ASSOCIATE_TAG='mytag-20'
